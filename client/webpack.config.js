25a696132fd3c3d4cba3e2e3c64843eabab8f654
Stephen Dzialo <stephendzialo@Stephens-MacBook-Pro.local>
2020-05-31 16:58:12 -0400

    changed front end port to 3000

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
       publicPath: '/',
    },
    devServer: {
-      historyApiFallback: true
+      historyApiFallback: true,
+      port: 3000
    },
    module: {
       rules: [
